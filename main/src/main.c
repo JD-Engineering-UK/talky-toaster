d3dfee9e81dc9db69e14d2163f3c3e10de0cf528
Jessica Doyle <frazzles740@gmail.com>
2025-02-21 22:30:27 +0000

    Trim down size to get it building again

--- main/src/main.c
+++ main/src/main.c
@@ -17,9 +17,9 @@ void boot_sequence()
     extern const uint8_t howdy_raw_end[] asm("_binary_howdy_raw_end");
     const size_t howdy_raw_size = (howdy_raw_end - howdy_raw_start);
 
-    extern const uint8_t im_talkie_raw_start[] asm("_binary_im_talkie_raw_start");
-    extern const uint8_t im_talkie_raw_end[] asm("_binary_im_talkie_raw_end");
-    const size_t im_talkie_raw_size = (im_talkie_raw_end - im_talkie_raw_start);
+    // extern const uint8_t im_talkie_raw_start[] asm("_binary_im_talkie_raw_start");
+    // extern const uint8_t im_talkie_raw_end[] asm("_binary_im_talkie_raw_end");
+    // const size_t im_talkie_raw_size = (im_talkie_raw_end - im_talkie_raw_start);
 
     queue_audio(boot_raw_size, boot_raw_start);
     queue_audio(boot_raw_size, boot_raw_start);
@@ -27,7 +27,7 @@ void boot_sequence()
     queue_audio(boot_raw_size, boot_raw_start);
     queue_audio(boot_raw_size, boot_raw_start);
     queue_audio(howdy_raw_size, howdy_raw_start);
-    queue_audio(im_talkie_raw_size, im_talkie_raw_start);
+    // queue_audio(im_talkie_raw_size, im_talkie_raw_start);
 }
 
 void app_main(void)
